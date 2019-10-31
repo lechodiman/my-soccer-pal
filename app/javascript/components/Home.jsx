33bfe9d4f6fa9bf7367f542f6bf06bea34426b40
Luis Chodiman <lechodiman@uc.cl>
2019-10-30 21:59:51 -0300

    :sparkles: Add react config

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Soccer Games</h1>
+        <p className="lead">A curated list of soccer games</p>
+        <hr className="my-4" />
+        <Link to="/games" className="btn btn-lg custom-button" role="button">
+          View Games
+        </Link>
+      </div>
+    </div>
+  </div>
+);
