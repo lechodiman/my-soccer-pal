dfb454ef3f943c30d939daf8b8629748514ed982
Luis Chodiman <lechodiman@uc.cl>
2019-10-31 23:18:33 -0300

    :art: Add routes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import GamesList from "../components/Games/GamesList";
+import GameDetails from "../components/Games/Details";
+import CreateGame from "../components/Games/CreateGame";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/games" component={GamesList}></Route>
+      <Route path="/games/:id" component={GameDetails}></Route>
+      <Route path="/games/new" component={CreateGame}></Route>
     </Switch>
   </Router>
 );
