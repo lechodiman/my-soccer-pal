043c30de2aa32a09eff2b0c283203bc59bb741e8
Luis Chodiman <lechodiman@uc.cl>
2019-11-04 19:06:30 -0300

    :children_crossing: Add cooler animations

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -4,14 +4,20 @@ import Home from "../components/Home";
 import GamesList from "../components/Games/GamesList";
 import GameDetails from "../components/Games/Details/GameDetails";
 import CreateGame from "../components/Games/CreateGame";
+import { AnimatedSwitch } from "react-router-transition";
 
 export default (
   <Router>
-    <Switch>
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
       <Route path="/" exact component={Home} />
       <Route path="/games" exact component={GamesList}></Route>
       <Route path="/games/details/:id" exact component={GameDetails}></Route>
       <Route path="/games/new" exact component={CreateGame}></Route>
-    </Switch>
+    </AnimatedSwitch>
   </Router>
 );
